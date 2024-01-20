423d6fafafd05aad3a9b2019ee7a6ef77a34a908
Kunstiception <lukas-bohl.de@outlook.de>
2024-01-20 12:47:35 +0100

    Dekorationen lassen sich jetzt in jeder Ecke des Rahmens positionieren

---
+++ Realtime3D_PA/Assets/Scripts/Controller/DecorationController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationController : MonoBehaviour
+{
+    /// <summary>
+    /// Verknüpfung zum FrameController-Skript
+    /// </summary>
+    public FrameController frameController;
+
+    /// <summary>
+    /// Ein Array mit den Dekorations-Prefabs
+    /// </summary>
+    public GameObject[] decorations;
+
+    /// <summary>
+    /// Die gerade geladene Dekoration
+    /// </summary>
+    public GameObject currentLoadedDecoration;
+
+    /// <summary>
+    /// Referenz zum Materialauswahl-Men�
+    /// </summary>
+    public GameObject decorationSelectionUI;
+
+    /// <summary>
+    /// Die Maße des gerade geladenen Rahmens
+    /// </summary>
+    private Vector3 frameSize;
+
+    /// <summary>
+    /// Die Position des gerade geladenen Rahmens
+    /// </summary>
+    private Vector3 framePosition;
+
+    /// <summary>
+    /// Die Position, an der die ausgwählte Dekoration gehängt werden soll
+    /// </summary>
+    private Vector3 decorationPosition;
+
+    /// <summary>
+    /// Ein Vektor zum exakten Platzieren der Dekoration
+    /// </summary>
+    private Vector3 offSet;
+
+    /// <summary>
+    /// Die Größe der aktuellen Dekoration
+    /// </summary>
+    private Vector3 decorationSize;
+
+    // Instanziert übden Index die ausgewählte Dekoration und platziert sie an der richtigen Stelle am Rahmen
+    public void SetDecoration(int index)
+    {
+        // Vorherige Dekoration entfernen
+        if (currentLoadedDecoration != null)
+        {
+            Destroy(currentLoadedDecoration);
+            currentLoadedDecoration = null;
+        }
+
+        // Dekoration anhand des ausgwählten Idex instanzieren
+        GameObject loadedDecoration = Instantiate(decorations[index]);
+
+        // Die geladene Deko wird in der neuen Variable gespeichert
+        currentLoadedDecoration = loadedDecoration;
+
+        // gefunden auf: https://discussions.unity.com/t/find-size-of-gameobject/6193/2
+        frameSize = frameController.currentLoadedFrame.GetComponent<Renderer>().bounds.size;
+
+        // gefunden auf: https://discussions.unity.com/t/find-size-of-gameobject/6193/2
+        decorationSize = currentLoadedDecoration.GetComponent<Renderer>().bounds.size;
+
+        // Ist der übergebene Index gültig
+        if (index == 0 && index <= decorations.Length - 1)
+        {
+
+
+            // die Position des aktuellen Rahmens als Vektor 3
+            framePosition = frameController.currentLoadedFrame.transform.position;
+
+            // Y holen gefunden auf: https://docs.unity3d.com/ScriptReference/Vector3.html
+            //An die richtige Position auf dem Rahmen setzen (in diesem Fall oben links)
+            offSet = new Vector3(frameSize.x/2 - decorationSize.x / 2, frameSize.y / 2 - decorationSize.y / 2, frameSize.z / 2);
+
+            // Von der Rahmenmitte aus wird mit dem Offset die richtige Position für die Dekoration ermittelt
+            currentLoadedDecoration.transform.position = framePosition + offSet;
+        }
+
+        // Ist der übergebene Index gültig
+        if (index == 1 && index <= decorations.Length - 1)
+        {
+
+
+            // die Position des aktuellen Rahmens als Vektor 3
+            framePosition = frameController.currentLoadedFrame.transform.position;
+
+            // Y holen gefunden auf: https://docs.unity3d.com/ScriptReference/Vector3.html
+            // An die richtige Position auf dem Rahmen setzen (in diesem Fall am Eck oben rechts)
+            offSet = new Vector3(-frameSize.x / 2 + decorationSize.x / 2, frameSize.y / 2 - decorationSize.y / 2, frameSize.z / 2);
+
+            // Von der Rahmenmitte aus wird mit dem Offset die richtige Position für die Dekoration ermittelt
+            currentLoadedDecoration.transform.position = framePosition + offSet;
+        }
+
+        // Ist der übergebene Index gültig
+        if (index == 2 && index <= decorations.Length - 1)
+        {
+
+
+            // die Position des aktuellen Rahmens als Vektor 3
+            framePosition = frameController.currentLoadedFrame.transform.position;
+
+            // Y holen gefunden auf: https://docs.unity3d.com/ScriptReference/Vector3.html
+            // An die richtige Position auf dem Rahmen setzen (in diesem Fall am Eck unten rechts)
+            offSet = new Vector3(-frameSize.x / 2 + decorationSize.x / 2, - frameSize.y / 2 + decorationSize.y / 2, frameSize.z / 2);
+
+            // Von der Rahmenmitte aus wird mit dem Offset die richtige Position für die Dekoration ermittelt
+            currentLoadedDecoration.transform.position = framePosition + offSet;
+        }
+
+        // Ist der übergebene Index gültig
+        if (index == 3 && index <= decorations.Length - 1)
+        {
+
+
+            // die Position des aktuellen Rahmens als Vektor 3
+            framePosition = frameController.currentLoadedFrame.transform.position;
+
+            // Y holen gefunden auf: https://docs.unity3d.com/ScriptReference/Vector3.html
+            //An die richtige Position auf dem Rahmen setzen (in diesem Fall unten links)
+            offSet = new Vector3(frameSize.x / 2 - decorationSize.x / 2, - frameSize.y / 2 + decorationSize.y / 2, frameSize.z / 2);
+
+            // Von der Rahmenmitte aus wird mit dem Offset die richtige Position für die Dekoration ermittelt
+            currentLoadedDecoration.transform.position = framePosition + offSet;
+        }
+    }
+}
