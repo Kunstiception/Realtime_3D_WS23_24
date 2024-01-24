f697f776bc58c077d3fc4f323c17fe869ef33d77
Kunstiception <lukas-bohl.de@outlook.de>
2024-01-25 00:17:50 +0100

    ImageRatio Mist

--- Realtime3D_PA/Assets/Scripts/ImageUpload/ImageUpload.cs
+++ Realtime3D_PA/Assets/Scripts/ImageUpload/ImageUpload.cs
@@ -7,11 +7,6 @@ using System.IO;
 
 public class ImageUpload : MonoBehaviour
 {
-    // basiert auf:
-    // https://www.reddit.com/r/gamedev/comments/dhw8ro/unity_c_allowing_a_player_to_import_an_image_mid/
-    // und:
-    // https://docs.unity3d.com/ScriptReference/Transform.SetParent.html
-
     // Das RawImage, auf das das Png kommt
     public RawImage image;
     // Das Kind-Objekt
@@ -28,7 +23,23 @@ public class ImageUpload : MonoBehaviour
     private GameObject newParent;
     // Refernz zum FrameController-Skript
     public FrameController frameController;
+    // Das hochgeladene Bild
+    public Texture userImage;
 
+    private float userImageWidth;
+
+    private float userImageHeight;
+
+    private float userImageAspect;
+
+    private Vector2 originalSize; 
+
+    private RawImage rawNewAspect;
+
+
+
+    // basiert auf:
+    // https://www.reddit.com/r/gamedev/comments/dhw8ro/unity_c_allowing_a_player_to_import_an_image_mid/
     private void Awake()
     {
         image = GetComponentInChildren<RawImage>();
@@ -51,19 +62,47 @@ public class ImageUpload : MonoBehaviour
         {
             WWW www = new WWW("file:///" + path);
             image.texture = www.texture;
+            // Speichert das Png als Textur ab
+            userImage = www.texture;
+            // https://docs.unity3d.com/ScriptReference/Texture-width.html
+            // Ermittel die Breite der Textur
+            userImageWidth = userImage.width;
+            // Ermittelt die Höhe der Textur
+            userImageHeight = userImage.height;
+            // Seitenverhältnis ermitteln mit Breite/Höhe
+            userImageAspect = userImageWidth / userImageHeight;
+            Debug.Log(userImageAspect);
+
+            // https://docs.unity3d.com/530/Documentation/ScriptReference/RectTransform-sizeDelta.html
+            originalSize = new Vector2(image.transform.localScale.x, image.transform.localScale.y);
+
+            image.rectTransform.sizeDelta = new Vector2(userImageWidth, userImageHeight);
+            // https://docs.unity3d.com/2018.3/Documentation/ScriptReference/UI.AspectRatioFitter-aspectRatio.html
+            image.GetComponent<AspectRatioFitter>().aspectRatio = userImageAspect;
+            Debug.Log(image.GetComponent<AspectRatioFitter>().aspectRatio);
+            
+            image.rectTransform.sizeDelta = new Vector2(originalSize.x, originalSize.y);
+            
+
+            //image.rectTransform.SizeToParent;
+
         }
     }
 
 
-    
 
+    // https://docs.unity3d.com/ScriptReference/Transform.SetParent.html
     public void SetNewParent()
     {
+        // Der Rahmen ist der gerade aktive Rahmen
         frame = frameController.currentLoadedFrame;
+        //
         newParent = frameController.currentLoadedFrame;
         // Sets "newParent" as the new parent of the child GameObject.
         parent = newParent;
+        // Diese Variable enthält die Position des gerade aktiven Rahmens
         framePosition = frame.transform.position;
+        // Das Kindobjekt wird an die Position des Rahmens gesetzt
         child.transform.position = framePosition;
     }
 }
